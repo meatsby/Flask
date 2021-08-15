5bc3560e723519fc2ee3bd2327bec5970e4ee7ad
정석윤 <meatsby@gmail.com>
2021-08-15 09:19:18 +0000

    21.08.15

--- application.py
+++ application.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 import sys
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello goorm!"
+    return render_template("hello.html")
+
+@application.route("/apply")
+def apply():
+    return render_template("apply.html")
+
+@application.route("/list")
+def list():
+    return render_template("list.html")
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=int(sys.argv[1]))
+    application.run(host='0.0.0.0')
