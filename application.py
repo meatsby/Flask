18cfbc0c3670009d53c84fd858073cbf883d756b
정석윤 <meatsby@gmail.com>
2021-08-15 13:25:28 +0000

    21.08.15

--- application.py
+++ application.py
@@ -1,26 +1,44 @@
-from flask import Flask, render_template, request
-import sys
+from flask import Flask, render_template, request, redirect, url_for
+import sys, database
 application = Flask(__name__)
+
 
 @application.route("/")
 def hello():
     return render_template("hello.html")
 
+
 @application.route("/apply")
 def apply():
     return render_template("apply.html")
 
+
 @application.route("/applyphoto")
-def applyphoto():
+def photo_apply():
     location = request.args.get("location")
-    clean = request.args.get("clean")
-    built = request.args.get("built")
-    print(location, clean, built)
-    # return render_template("applyphoto.html")
+    cleaness = request.args.get("clean")
+    built_in = request.args.get("built")
+    if cleaness == None:
+        cleaness = False
+    else:
+        cleaness = True
+    database.save(location, cleaness, built_in)
+    return render_template("apply_photo.html")
+
+
+@application.route("/upload_done", methods=["POST"])
+def upload_done():
+    uploaded_files = request.files["file"]
+    uploaded_files.save("static/img/{}.jpeg".format(database.now_index()))
+    return redirect(url_for("hello"))
+
 
 @application.route("/list")
 def list():
-    return render_template("list.html")
+    house_list = database.load_list()
+    length = len(house_list)
+    return render_template("list.html", house_list, length=length)
+
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
